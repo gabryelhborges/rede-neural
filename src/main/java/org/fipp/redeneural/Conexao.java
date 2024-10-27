260170bf8c881145b008b7e39533dcb8fcc25586
byelBorges <gabryelhenborges@gmail.com>
2024-10-26 23:50:15 -0300

    Modelos: RedeNeural, Neuronio e Conexao

---
+++ src/main/java/org/fipp/redeneural/Conexao.java
@@ -0,0 +1,38 @@
+package org.fipp.redeneural;
+
+public class Conexao {
+    private double peso;
+    private Neuronio origem;
+    private Neuronio destino;
+
+    public Conexao(Neuronio origem, Neuronio destino, double peso) {
+        this.origem = origem;
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Neuronio getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(Neuronio origem) {
+        this.origem = origem;
+    }
+
+    public Neuronio getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Neuronio destino) {
+        this.destino = destino;
+    }
+
+}
---
+++ src/main/java/org/fipp/redeneural/Neuronio.java
@@ -0,0 +1,51 @@
+package org.fipp.redeneural;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Neuronio {
+    private double net;
+    private double i;//saida
+    private double erro;
+    private List<Conexao> conexoes;//representa os neuronios destino
+
+    private void adicionaConexao(Conexao c){
+        this.conexoes.add(c);
+    }
+
+    public Neuronio(){
+        conexoes = new ArrayList<>();
+    }
+
+    public double getNet() {
+        return net;
+    }
+
+    public void setNet(double net) {
+        this.net = net;
+    }
+
+    public double getI() {
+        return i;
+    }
+
+    public void setI(double i) {
+        this.i = i;
+    }
+
+    public double getErro() {
+        return erro;
+    }
+
+    public void setErro(double erro) {
+        this.erro = erro;
+    }
+
+    public List<Conexao> getConexoes() {
+        return conexoes;
+    }
+
+    public void setConexoes(List<Conexao> conexoes) {
+        this.conexoes = conexoes;
+    }
+}
